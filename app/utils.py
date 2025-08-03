2af91385b77d40ecd78cf629f57825576223d8e2
Ai Afrocentrée <ai.afrocentree@gmail.com>
2025-08-03 17:38:33 +0200

    Update utils.py
    Refonte V3

--- app/utils.py
+++ app/utils.py
@@ -1,1 +1,8 @@
-ss
+# app/utils.py
+import os
+import json
+
+def load_json_data(filename):
+    base_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'data', filename)
+    with open(base_path, 'r', encoding='utf-8') as f:
+        return json.load(f)
