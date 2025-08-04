4e86f36b199a51d07ec7683d5f9b85cbfad4026e
Ai Afrocentrée <ai.afrocentree@gmail.com>
2025-08-04 11:38:39 +0200

    Update __init__.py
    correction suite à erreur déploiement render

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,4 @@
 # app/__init__.py
-# Point d'entrée de l'application Flask
-
 from flask import Flask
 from config import Config
 
@@ -8,7 +6,8 @@ def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
 
-    from app.main import app as main_blueprint
+    # Enregistre le blueprint
+    from app.main import main_blueprint
     app.register_blueprint(main_blueprint)
 
     return app
