30d9adb6c883f2a68bc9b0d1970255fec83e0d91
Ai Afrocentrée <ai.afrocentree@gmail.com>
2025-08-04 15:18:27 +0200

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -5,9 +5,6 @@ from config import Config
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
-
-    # Enregistre le blueprint
     from app.main import main_blueprint
     app.register_blueprint(main_blueprint)
-
     return app
